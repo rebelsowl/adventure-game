55dc84c8c7e1f71dd8725f82cb32073a86bd8583
M <rebelsowl53@gmail.com>
2022-11-14 20:47:18 +0300

    remove woodcutting transparency

--- src/main/java/com/mae/tile/interactive/InteractiveTile.java
+++ src/main/java/com/mae/tile/interactive/InteractiveTile.java
@@ -1,8 +1,11 @@
 package com.mae.tile.interactive;
 
+import com.mae.config.Settings;
 import com.mae.entity.Entity;
 import com.mae.panel.GamePanel;
 import lombok.Data;
+
+import java.awt.*;
 
 import static com.mae.config.Settings.TILE_SIZE;
 
@@ -40,6 +43,22 @@ public abstract class InteractiveTile extends Entity {
                 invincibleCounter = 0;
             }
         }
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+        int screenX = worldX - gp.player.getWorldX() + gp.player.getScreenX();
+        int screenY = worldY - gp.player.getWorldY() + gp.player.getScreenY();
+
+        if (worldX + Settings.TILE_SIZE > gp.player.getWorldX() - gp.player.getScreenX() &&
+                worldX - Settings.TILE_SIZE < gp.player.getWorldX() + gp.player.getScreenX() &&
+                worldY + Settings.TILE_SIZE > gp.player.getWorldY() - gp.player.getScreenY() &&
+                worldY - Settings.TILE_SIZE < gp.player.getWorldY() + gp.player.getScreenY()) { // Only draw the tiles around the player
+
+            g2.drawImage(down1, screenX, screenY, null);
+
+        }
+
     }
 
 
